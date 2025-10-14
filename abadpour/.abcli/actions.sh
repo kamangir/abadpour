4dbb549eca7edd6a8fd6c68ba134e8d74e9978ad
kamangir <arash@kamangir.net>
2025-10-14 11:14:57 +0330

    rebuild - kamangir/bolt#746

--- abadpour/.abcli/actions.sh
+++ abadpour/.abcli/actions.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
 function abadpour_action_git_before_push() {
+    bluer_plugin build_README
+    [[ $? -ne 0 ]] && return 1
+
     [[ "$(bluer_ai_git get_branch)" != "main" ]] &&
         return 0
 
